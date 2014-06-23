e1da72b6193d6c4040b92bf5ebff5166cb8f20d1
Abhishek280187 <abhishek280187@gmail.com>
2014-06-23 14:34:16 +0530

    For loop extended to 4000 iteration

--- src/TestClassTest.java
+++ src/TestClassTest.java
@@ -9,7 +9,7 @@ public class TestClassTest {
 	public void testAdd() {
 		assertEquals(50, TestClass.add(20, 30));
 		int i = 0;
-		for (i = 0; i < 3000; i++){
+		for (i = 0; i < 4000; i++){
 			assertEquals(i, TestClass.add(i-1, 1));
 		}
 	}
